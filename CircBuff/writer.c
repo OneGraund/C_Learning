1b9caf32f85a1329de0d5876539318d445a3162e
OneGraund <vovangaunder@gmail.com>
2024-11-12 10:46:54 +0100

    make push commit

--- CircBuff/writer.c
+++ CircBuff/writer.c
@@ -7,22 +7,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+
+int shm_fd;
+SharedBuffer *shared_buffer;
+sem_t *res_free, *used;
 
 void error_handle(void) {
 	fprintf(stdout, "An error occured: %s\n", strerror(errno));
 	exit(EXIT_FAILURE);
 }
 
-void write_value(SharedBuffer *shared_buffer, sem_t *free, sem_t *used, int val) {
-	if (sem_wait(free) == -1) error_handle();
-	/* If free reaches 0, it means the buffer is full, and any additional
-	   attempt by the writer to sem_wait(free) will block until a reader
-	   frees up a slot by reading a value and calling sem_post(free) */
+void write_value(SharedBuffer *shared_buffer, sem_t *res_free, sem_t *used, int val) {
+	if (sem_wait(res_free) == -1) error_handle();
+	/* If res_free reaches 0, it means the buffer is full, and any additional
+	   attempt by the writer to sem_wait(res_free) will block until a reader
+	   res_frees up a slot by reading a value and calling sem_post(res_free) */
 	shared_buffer->buf[shared_buffer->wr_pos] = val;
 	shared_buffer->wr_pos = (shared_buffer->wr_pos + 1) % BUF_LEN;
 	if (sem_post(used) == -1) error_handle();
 }
 
+void free_resources(SharedBuffer *shared_buffer, sem_t *res_free, sem_t *used) {
+	printf("Freeing resources\n");
+	if (munmap(shared_buffer, sizeof(SharedBuffer)) == -1) error_handle();
+	if (close(shm_fd) == -1) error_handle();
+ 	if (sem_close(res_free) == -1) error_handle();
+	if (sem_close(used) == -1) error_handle();
+	if (shm_unlink(SHARED_MEM_NAME) == -1) error_handle();
+	if (shm_unlink(SEMAPHORE_FREE_NAME) == -1) error_handle();
+	if (shm_unlink(SEMAPHORE_USED_NAME) == -1) error_handle();
+}
 
 
 int main(int argc, char **argv) {
@@ -36,20 +51,28 @@ int main(int argc, char **argv) {
 		MAP_SHARED, shm_fd, 0);
 	if (shared_buffer == MAP_FAILED) error_handle();
 
-	sem_t *free = sem_open(SEMAPHORE_FREE_NAME, O_CREAT, 0666, BUF_LEN); // all slots free
+	sem_t *res_free = sem_open(SEMAPHORE_FREE_NAME, O_CREAT, 0666, BUF_LEN); // all slots free
 	sem_t *used = sem_open(SEMAPHORE_USED_NAME, O_CREAT, 0666, 0); // no used
-	if (free == SEM_FAILED || used == SEM_FAILED) error_handle();
+	if (res_free == SEM_FAILED || used == SEM_FAILED) error_handle();
+
+	
+	void handle_signal(int signal) {
+		free_resources(shared_buffer, res_free, used);
+		exit(0);
+	}
+
+	struct sigaction sa;
+	memset(&sa,	0, sizeof(sa));
+	sa.sa_handler = handle_signal;
+	sigaction(SIGINT, &sa, NULL);
 
 	for (int i=1; i<=10; i++) {
 		printf("Writer: writing %d to buffer\n", i);
-		write_value(shared_buffer, free, used, i);
+		write_value(shared_buffer, res_free, used, i);
 		sleep(1);
 	}
 
-	if (munmap(shared_buffer, sizeof(SharedBuffer)) == -1) error_handle();
-	if (close(shm_fd) == -1) error_handle();
-	if (sem_close(free) == -1) error_handle();
-	if (sem_close(used) == -1) error_handle();
+	free_resources(shared_buffer, res_free, used);
 
 	return 0;
 }
